67699ba7af085da3f91a24b002abdaec59701f33
VictorCesarM <victor.cesar.m@gmail.com>
2017-11-14 23:02:26 -0300

    Got forms working properly and added delete task button

--- src/pages/information-modal/information-modal.module.ts
+++ src/pages/information-modal/information-modal.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { InformationModalPage } from './information-modal';
-import { Storage } from '@ionic/storage';
 
 @NgModule({
   declarations: [
     InformationModalPage,
   ],
   imports: [
-    IonicStorageModule.forRoot(),
     IonicPageModule.forChild(InformationModalPage),
   ],
 })
